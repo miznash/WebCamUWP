106b396fbe64c2050222b8312ecf2e5672f91c25
miznash <sugino41@outlook.com>
2020-01-21 00:50:45 +0900

    Webカメラのスルー画表示を実装

--- WebCamUWP/MainPage.xaml.cs
+++ WebCamUWP/MainPage.xaml.cs
@@ -12,8 +12,18 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Core;
+using Windows.Media.Capture;
+using Windows.ApplicationModel;
+using System.Threading.Tasks;
+using Windows.System.Display;
+using Windows.Graphics.Display;
+using Windows.UI.Popups;
 
 // 空白ページの項目テンプレートについては、https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x411 を参照してください
+
+// Webカメラのプレビューについては以下を参考にした。
+// https://docs.microsoft.com/ja-jp/windows/uwp/audio-video-camera/simple-camera-preview-access
 
 namespace WebCamUWP
 {
@@ -25,6 +35,101 @@ namespace WebCamUWP
         public MainPage()
         {
             this.InitializeComponent();
+            Application.Current.Suspending += Application_Suspending;
         }
+        private async Task StartPreviewAsync()
+        {
+            try
+            {
+                // Package.appxmanifestで、カメラだけでなくマイクも有効にしないと例外が発生。
+                // Windowsの「カメラのプライバシー設定」で、このアプリのカメラアクセスをONにしておかないと例外が発生。
+                mediaCapture = new MediaCapture();
+                await mediaCapture.InitializeAsync();
+
+                displayRequest.RequestActive();
+                DisplayInformation.AutoRotationPreferences = DisplayOrientations.Landscape;
+            }
+            catch (UnauthorizedAccessException)
+            {
+                // This will be thrown if the user denied access to the camera in privacy settings
+                await new MessageDialog("The app was denied access to the camera", "Exception").ShowAsync();
+                return;
+            }
+
+            try
+            {
+                PreviewControl.Source = mediaCapture;
+                await mediaCapture.StartPreviewAsync();
+                isPreviewing = true;
+            }
+            catch (System.IO.FileLoadException)
+            {
+                mediaCapture.CaptureDeviceExclusiveControlStatusChanged += _mediaCapture_CaptureDeviceExclusiveControlStatusChanged;
+            }
+
+        }
+        private async void _mediaCapture_CaptureDeviceExclusiveControlStatusChanged(MediaCapture sender, MediaCaptureDeviceExclusiveControlStatusChangedEventArgs args)
+        {
+            if (args.Status == MediaCaptureDeviceExclusiveControlStatus.SharedReadOnlyAvailable)
+            {
+                await new MessageDialog("The camera preview can't be displayed because another app has exclusive access", "Error").ShowAsync();
+            }
+            else if (args.Status == MediaCaptureDeviceExclusiveControlStatus.ExclusiveControlAvailable && !isPreviewing)
+            {
+                await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, async () =>
+                {
+                    await StartPreviewAsync();
+                });
+            }
+        }
+
+        private async Task CleanupCameraAsync()
+        {
+            if (mediaCapture != null)
+            {
+                if (isPreviewing)
+                {
+                    await mediaCapture.StopPreviewAsync();
+                }
+
+                await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                {
+                    PreviewControl.Source = null;
+                    if (displayRequest != null)
+                    {
+                        displayRequest.RequestRelease();
+                    }
+
+                    mediaCapture.Dispose();
+                    mediaCapture = null;
+                });
+            }
+
+        }
+        protected async override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            await CleanupCameraAsync();
+        }
+
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            await StartPreviewAsync();
+        }
+
+
+        private async void Application_Suspending(object sender, SuspendingEventArgs e)
+        {
+            // Handle global application events only if this page is active
+            if (Frame.CurrentSourcePageType == typeof(MainPage))
+            {
+                var deferral = e.SuspendingOperation.GetDeferral();
+                await CleanupCameraAsync();
+                deferral.Complete();
+            }
+        }
+
+        MediaCapture mediaCapture;
+        private bool isPreviewing;
+        DisplayRequest displayRequest = new DisplayRequest();
     }
 }
